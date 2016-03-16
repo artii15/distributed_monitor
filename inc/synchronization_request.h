7d1adf4705d7fff4fcb9f02c4ca150b8c0dd9dec
Artii15 <artur.kowalski15@gmail.com>
2016-03-16 21:45:12 +0100

    Network reprezentation

--- inc/synchronization_request.h
+++ inc/synchronization_request.h
@@ -7,11 +7,11 @@ class synchronization_request {
 	public:
 		uint32_t time;
 		uint32_t pid;
-		uint8_t tag;
+		uint16_t tag;
 
 		synchronization_request();
 		synchronization_request(char*);
-		synchronization_request(uint32_t time, uint32_t pid, uint32_t tag);
+		synchronization_request(uint32_t time, uint32_t pid, uint16_t tag);
 
 		bool operator>(const synchronization_request& request) const;
 
--- src/synchronization_request.cpp
+++ src/synchronization_request.cpp
@@ -1,6 +1,7 @@
 #include "../inc/synchronization_request.h"
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 
 synchronization_request::synchronization_request() {
 	this->time = 0;
@@ -11,16 +12,22 @@ synchronization_request::synchronization_request() {
 synchronization_request::synchronization_request(char* serialized) {
 	char* seek = serialized;
 
+	uint32_t time;
 	memcpy(&time, seek, sizeof(time));
+	this->time = ntohl(time);
 	seek += sizeof(time);
 
+	uint32_t pid;
 	memcpy(&pid, seek, sizeof(pid));
+	this->pid = ntohl(pid);
 	seek += sizeof(pid);
 
+	uint16_t tag;
 	memcpy(&tag, seek, sizeof(tag));
+	this->tag = ntohs(tag);
 }
 
-synchronization_request::synchronization_request(uint32_t time, uint32_t pid, uint32_t tag) {
+synchronization_request::synchronization_request(uint32_t time, uint32_t pid, uint16_t tag) {
 	this->time = time;
 	this->pid = pid;
 	this->tag = tag;
@@ -42,12 +49,15 @@ char* synchronization_request::serialize() {
 	char* buf = (char*)malloc(sizeof(time) + sizeof(pid) + sizeof(tag));
 	char* seek = buf;
 	
+	uint32_t time = htonl(this->time);
 	memcpy(seek, &time, sizeof(time));
 	seek += sizeof(time);
 
+	uint32_t pid = htonl(this->pid);
 	memcpy(seek, &pid, sizeof(pid));
 	seek += sizeof(pid);
 
+	uint16_t tag = htons(this->tag);
 	memcpy(seek, &tag, sizeof(tag));
 
 	return buf;
