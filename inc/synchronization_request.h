ff90fa7b50b2d101bc1a158e8630596f6e152bf0
Artii15 <artur.kowalski15@gmail.com>
2016-03-18 22:21:30 +0100

    Redundant method removed

--- inc/synchronization_request.h
+++ inc/synchronization_request.h
@@ -16,9 +16,7 @@ class synchronization_request {
 		synchronization_request(uint32_t time, uint32_t pid, uint16_t tag, uint32_t creation_time, uint16_t guarded_section_id);
 
 		bool operator>(const synchronization_request& request) const;
-
 		bool operator==(const synchronization_request& request) const;
-
 		bool operator<(const synchronization_request& request) const;
 
 		char* serialize();
--- src/communicator.cpp
+++ src/communicator.cpp
@@ -7,7 +7,7 @@ communicator::communicator(uint32_t process_id) {
 
 void communicator::send_lock_request(guarded_section_descriptor section_descriptor) {
 	synchronization_request request(time, process_id, REQUEST_TAG::LOCK_REQUEST, time, section_descriptor.guarded_section_id);
-	lock_requests[request->guarded_section_id].push(*request);
+	lock_requests[request.guarded_section_id].push(request);
 
 	broadcast_sync_request(&request);
 }
