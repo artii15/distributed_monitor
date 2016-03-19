bd38ed286b7cbd08f43921923857eb5433f7eba0
Artii15 <artur.kowalski15@gmail.com>
2016-03-19 16:22:44 +0100

    Datastructures for holding descriptors created

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "inc/monitor.h"
-#include "inc/request_descriptor.h"
 #include "inc/mpi_communicator.h"
-#include "inc/synchronization_request.h"
 #include <mpi.h>
-#include <map>
 #include <iostream>
 
 using namespace std;
@@ -19,12 +16,6 @@ int main(int argc, char** argv) {
 
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	synchronization_request r1, r2;
-	map<synchronization_request, request_descriptor> desc;
-
-	//desc[r1] = request_descriptor(r1, NULL, 0);
-
 
 	mpi_communicator comm(world_rank, world_size);
 
