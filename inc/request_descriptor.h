0a70902e4b64e2e898c1508747bc2f542655c654
Artii15 <artur.kowalski15@gmail.com>
2016-03-19 15:57:40 +0100

    Copying requests instead of holding pointers

--- inc/request_descriptor.h
+++ inc/request_descriptor.h
@@ -6,11 +6,11 @@
 
 class request_descriptor {
 	public:
-		synchronization_request* request;
+		synchronization_request request;
 		pthread_mutex_t* mutex;
 		unsigned int number_of_confirmations;
 
-		request_descriptor(synchronization_request* request, pthread_mutex_t* mutex, unsigned int number_of_confirmations);
+		request_descriptor(synchronization_request& request, pthread_mutex_t* mutex, unsigned int number_of_confirmations);
 
 		bool operator>(const request_descriptor& descriptor) const;
 };
--- src/request_descriptor.cpp
+++ src/request_descriptor.cpp
@@ -1,11 +1,11 @@
 #include "../inc/request_descriptor.h"
 
-request_descriptor::request_descriptor(synchronization_request* request, pthread_mutex_t* mutex, unsigned int number_of_confirmations) {
+request_descriptor::request_descriptor(synchronization_request& request, pthread_mutex_t* mutex, unsigned int number_of_confirmations) {
 	this->request = request;
 	this->mutex = mutex;
 	this->number_of_confirmations = number_of_confirmations;
 }
 
 bool request_descriptor::operator>(const request_descriptor& descriptor) const {
-	return *request > *descriptor.request;
+	return request > descriptor.request;
 }
