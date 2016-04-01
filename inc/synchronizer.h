d36681b14be1b61bc78b1c5c57fed4c864d5f7cc
Artii15 <artur.kowalski15@gmail.com>
2016-04-01 22:58:23 +0200

    Queues moved to synchronizer class

--- inc/synchronizer.h
+++ inc/synchronizer.h
@@ -1,11 +1,29 @@
 #ifndef SYNCHRONIZER_H
 #define SYNCHRONIZER_H
 
+#include <map>
+#include <set>
+#include <pthread.h>
+#include "request_descriptor.h"
 #include "message_listener.h"
+#include "messages/lock_request.h"
+#include "messages/lock_response.h"
+#include "messages/release_signal.h"
+#include "messages/wait_signal.h"
+#include "messages/wake_signal.h"
 
 class synchronizer: public message_listener {
 	public:
 		virtual void on_message(frame*);
+	
+	private:
+		std::map<uint16_t, std::set<lock_request> > lock_requests;
+		std::map<lock_request, request_descriptor> requests_descriptors;
+		std::map<uint16_t, const lock_request*> own_requests;
+
+		std::map<uint16_t, std::set<wait_signal> > wait_signals;
+		std::map<wait_signal, pthread_mutex_t*> wait_signals_mutexes;
+		std::map<uint16_t, const wait_signal*> own_wait_signals;
 };
 
 #endif
