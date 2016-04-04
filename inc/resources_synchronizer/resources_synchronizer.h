530ca04326dc9fff25f02e75b87bc8d8270408df
Artii15 <artur.kowalski15@gmail.com>
2016-04-04 10:38:47 +0200

    Critical section secured using mutex

--- inc/resources_synchronizer/resources_synchronizer.h
+++ inc/resources_synchronizer/resources_synchronizer.h
@@ -3,12 +3,14 @@
 
 #include "resources.h"
 #include <map>
+#include <pthread.h>
 
 class resources_synchronizer {
 	public:
 		void register_resources(resources*);
 	private:
 		std::map<uint16_t, resources*> sections_resources;
+		pthread_mutex_t internal_state_mutex = PTHREAD_MUTEX_INITIALIZER;
 };
 
 #endif
--- src/resources_synchronizer/resources_synchronizer.cpp
+++ src/resources_synchronizer/resources_synchronizer.cpp
@@ -1,5 +1,9 @@
 #include "../../inc/resources_synchronizer/resources_synchronizer.h"
 
 void resources_synchronizer::register_resources(resources* section_resources) {
+	pthread_mutex_lock(&internal_state_mutex);
+
 	sections_resources[section_resources->critical_section_id] = section_resources;	
+
+	pthread_mutex_unlock(&internal_state_mutex);
 }
