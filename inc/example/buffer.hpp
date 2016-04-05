019e535368b02225807c9fefc11b4241f56640fd
Artii15 <artur.kowalski15@gmail.com>
2016-04-05 17:47:11 +0200

    Buffer initialization

--- inc/example/buffer.hpp
+++ inc/example/buffer.hpp
@@ -2,23 +2,32 @@
 #define BUFFER_HPP
 
 #include <stdint.h>
+#include <stdlib.h>
 
 template <class T>
 class buffer {
 	public:
-		buffer(uint32_t size) {
+		buffer(uint32_t buf_size) {
+			this->buf_size = buf_size;
+			number_of_elements = 0;
+			elements = (T*)calloc(buf_size, sizeof(T));
+		}
+
+		~buffer() {
 
 		}
 
 		uint32_t count() {
-			return 0;
+			return number_of_elements;
 		}
 
-		uint32_t get_capacity() {
-			return 0;
+		uint32_t capacity() {
+			return buf_size;
 		}
 	private:
-		uint32_t size;
+		uint32_t buf_size;
+		uint32_t number_of_elements;
+		T* elements;
 };
 
 #endif
